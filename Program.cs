f5150123221ca5e791ad29f01e1d90b68ce87e71
Admin <elenaver1984@ya.ru>
2023-05-23 10:17:23 +0500

    Task02

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+int number1=Convert.ToInt32(Console.ReadLine());
+int number2=Convert.ToInt32(Console.ReadLine());
+int max=number1;
+
+if (number2>max)
+{
+    Console.WriteLine($"max = {number2}, min = {number1}");
+}
+else
+{
+    Console.WriteLine($"max ={number1}, min = {number2}");
+}
